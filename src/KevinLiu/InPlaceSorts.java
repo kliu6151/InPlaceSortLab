c02356649abbdcd6566731377df4a93c0bb408d9
BT_1N3_22 <@>
2018-11-19 11:28:22 -0500

    I was here

--- src/KevinLiu/InPlaceSorts.java
+++ src/KevinLiu/InPlaceSorts.java
@@ -101,5 +101,24 @@ public class InPlaceSorts {
         return arr;
     }
 
+    public static String[] randomStringArr(int num, int length)
+    {
+        String [] arr = new String [num];
+        while(num>0)
+        {
+            int i =0;
+            String s = "";
+            while(i<length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s + c;
+                i++;
+            }
+            num--;
+            arr[num]=s;
+        }
+        return arr;
+    }
 
 }
+
--- src/KevinLiu/Runner.java
+++ src/KevinLiu/Runner.java
@@ -5,21 +5,27 @@ import java.util.Arrays;
 public class Runner {
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
-        int[] arr = InPlaceSorts.randIntArr(500);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = InPlaceSorts.randIntArr(50000);
+        System.out.println(Arrays.toString(arr) + "\n Processing...");
         InPlaceSorts.insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr) + "\n Done");
         time = System.currentTimeMillis() - time;
         System.out.println("Time taken: " + time * 0.001 + " seconds");
         System.out.println("---------------");
-        double[] arrDouble = InPlaceSorts.randDoubleArr(500);
-        System.out.println(Arrays.toString(arrDouble));
+        double[] arrDouble = InPlaceSorts.randDoubleArr(50000);
+        time = System.currentTimeMillis();
+        System.out.println(Arrays.toString(arrDouble) + "\n Processing...");
         InPlaceSorts.selectionSort(arrDouble);
-        System.out.println(Arrays.toString(arrDouble));
+        System.out.println(Arrays.toString(arrDouble) + "\n Done");
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time taken: " + time * 0.001 + " seconds");
         System.out.println("---------------");
-        String[] arrString = {"always", "American","among","hi","beep","zzep","zoo","bye"};
-        System.out.println(Arrays.toString(arrString));
+        String[] arrString = InPlaceSorts.randomStringArr(50000, 4);
+        time = System.currentTimeMillis();
+        System.out.println(Arrays.toString(arrString) + "\n Processing...");
         InPlaceSorts.bubbleSort(arrString);
-        System.out.println(Arrays.toString(arrString));
+        System.out.println(Arrays.toString(arrString) + "\n Done");
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time taken: " + time * 0.001 + " seconds");
     }
 }
